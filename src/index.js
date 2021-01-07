e145aaefe4b9c96bdd9397316c00f16c2f950271
Ajay <ajayaj7397@psnacet.edu.in>
2021-01-07 14:31:12 +0530

    rout update

--- src/index.js
+++ src/index.js
@@ -51,7 +51,7 @@ const Root = () => {
 export const RouterRoot = (props) => {
     return (
         <Provider store={store}>
-            <Router>
+            <Router basename="/Fire-Chat/">
                 {props.children}
             </Router>
         </Provider>
