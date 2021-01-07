771a8c81426e0a04a00bfe0a25e98f060d3cf0d7
Ajay <ajayaj7397@psnacet.edu.in>
2021-01-07 18:49:11 +0530

    hash router changed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useHistory, BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useHistory, HashRouter as Router, Switch, Route } from "react-router-dom";
 import ReactDOM from 'react-dom';
 
 import { createBrowserHistory } from 'history';
@@ -53,7 +53,7 @@ const Root = () => {
 export const RouterRoot = (props) => {
     return (
         <Provider store={store}>
-            <Router basename="/#/Fire-Chat" history={history}>
+            <Router basename="/#/Fire-Chat" /*history={history}*/>
                 {props.children}
             </Router>
         </Provider>
